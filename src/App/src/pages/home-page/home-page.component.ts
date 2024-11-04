692f08d673d4f27b1209ceed9d8d93596425d3d4
vish <mvishnurana@gmail.com>
2024-11-05 04:34:21 +0800

    moved global styles element level styles to application styles

--- src/App/src/pages/home-page/home-page.component.ts
+++ src/App/src/pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AsyncPipe, CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { TodoService } from '../../app/services/todo.service';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [AsyncPipe, CommonModule, MatCardModule],
+  imports: [AsyncPipe, CommonModule, MatButtonModule, MatCardModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
